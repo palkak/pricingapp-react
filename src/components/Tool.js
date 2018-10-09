e60f057669a7f63c4594240f1829e844900141ce
Krzysztof <lamuerte@op.pl>
2018-10-09 21:48:57 +0200

    Market component added

--- src/components/Tool.js
+++ src/components/Tool.js
@@ -1,10 +1,19 @@
 import React from "react";
 import {Grid} from 'react-bootstrap';
+import {Market} from "./Market";
+
+const markets = [
+  {name: 'UK'},
+  {name: 'DE'},
+  {name: 'IT'},
+  {name: 'ES'}
+];
 
 const Tool = () => (
 
   <Grid>
     <h1>tu ma byc wyceniarka</h1>
+    {markets.map(i => <Market name={i.name}/>)}
   </Grid>
 
 );
